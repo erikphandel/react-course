31cb3feb39be4ef15dc520eb61d2c74043467a24
Erik Handel <erikphandel@gmail.com>
2026-01-13 18:04:52 -0300

    Added update cart item quantity to the Checkout page

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,10 @@
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 import axios from "axios";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
@@ -21,11 +24,34 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">
-              {cartItem.quantity}
-            </span>
+            Quantity:
+            {isUpdating ? (
+              <input type="number" className="quantity-input" 
+              value={quantity} 
+              onChange={(e) => {setQuantity(e.target.value)}}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {(async () => {
+                  await axios.put(`/api/cart-items/${cartItem.productId}`, { quantity: Number(quantity) });
+                  await loadCart();
+                  setIsUpdating(false);
+                })() } else if (e.key === 'Escape') {
+                  setIsUpdating(false);
+                  setQuantity(cartItem.quantity);
+                }
+              }} />
+            ) : (
+              <span className="quantity-label">
+                {cartItem.quantity}
+              </span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">
+          <span className="update-quantity-link link-primary" onClick={
+            isUpdating ?
+              async () => {
+                await axios.put(`/api/cart-items/${cartItem.productId}`, { quantity: Number(quantity) });
+                await loadCart();
+                setIsUpdating(false);
+              } : () => setIsUpdating(true)}>
             Update
           </span>
           <span className="delete-quantity-link link-primary"
